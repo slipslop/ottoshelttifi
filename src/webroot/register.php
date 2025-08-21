d63f214cf6fc128f9a227ac2ffe4d3550b976200
slipslop <vili.takaanttila@gmail.com>
2025-08-21 17:37:45 +0000

    Start register page

---
+++ src/webroot/register.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+?>
+
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <meta charset="UTF-8">
+    <title>Register</title>
+  </head>
+  <body>
+    <div class="content">
+	<h1>Register</h1>
+    </div>
+  </body>
+</html>
+
