3fb5c3fda6ab56f73153bee1ca4bccc3b5a9b591
slipslop <vili.takaanttila@gmail.com>
2025-08-22 16:26:12 +0300

    Process POST requests in register

--- src/webroot/register.php
+++ src/webroot/register.php
@@ -1,15 +1,30 @@
 <?php
+
 declare(strict_types=1);
 
 require_once('../includes/errors.php');
 require_once('../includes/template.php');
+require_once('../includes/request.php');
 
 function renderRegister(): void
 {
-  require_once('../templates/register.php');
+    require_once('../templates/register.php');
+}
+
+requestAllowMethods(['GET', 'POST']);
+
+$user = [
+    'username' => null,
+    'password' => null,
+];
+
+if (requestMethodIs('POST')) {
+    $user = requestGetPostParameters([
+        'username' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        'password' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+    ]);
 }
 
 renderHeader();
 renderRegister();
 renderFooter();
-?>
