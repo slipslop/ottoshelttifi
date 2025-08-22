d544f91bb9e96ed792255661355958af97b8e7db
slipslop <vili.takaanttila@gmail.com>
2025-08-22 14:49:34 +0300

    Add template.php library file

---
+++ src/includes/template.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+function renderHeader(): void
+{
+    require_once('../templates/header.php');
+}
+
+function renderFooter(): void
+{
+    require_once('../templates/footer.php');
+}
