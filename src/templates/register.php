0c18e68b0582ea42d0087090d7da65afba9d5735
slipslop <vili.takaanttila@gmail.com>
2025-08-26 15:43:11 +0300

    Improve styles of register template

--- src/templates/register.php
+++ src/templates/register.php
@@ -1,26 +1,62 @@
-<?php ?>
-
 <main>
     <div class="container">
         <div class="form-container">
-            <form action="" method="post">
+            <form method="post">
                 <input type="hidden" name="csrf_token" value="<?=authGetCSRF()?>">
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username" required max="32" value="<?=$user['username'] ?? ''?>">
-                <?php if (isset($errors['username'])): ?>
-                    <span class="error"><?=$errors['username'];?></span>
-                <?php endif; ?>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" required maxlength="64">
-                <?php if (isset($errors['password'])): ?>
-                    <span class="error"><?=$errors['password'];?></span>
-                <?php endif; ?>
-                <label for="password">Confirm password:</label>
-                <input type="password" name="password_confirm" id="password_confirm" required maxlength="64">
-                <?php if (isset($errors['password_confirm'])): ?>
-                    <span class="error"><?=$errors['password_confirm'];?></span>
-                <?php endif; ?>
-                <input type="submit" value="Register">
+
+                <div class="form-group">
+                    <label for="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        required
+                        maxlength="32"
+                        aria-describedby="username-error"
+                        value="<?= $user['username'] ?? '' ?>">
+
+                    <?php if (isset($errors['username'])): ?>
+                        <span class="error" id="username-error" role="alert">
+                            <?= $errors['username']; ?>
+                        </span>
+                    <?php endif; ?>
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                        maxlength="64"
+                        aria-describedby="password-error">
+
+                    <?php if (isset($errors['password'])): ?>
+                        <span class="error" id="password-error" role="alert">
+                            <?= $errors['password']; ?>
+                        </span>
+                    <?php endif; ?>
+                </div>
+
+                <div class="form-group">
+                    <label for="password_confirm">Confirm password:</label>
+                    <input
+                        type="password"
+                        id="password_confirm"
+                        name="password_confirm"
+                        required
+                        maxlength="64"
+                        aria-describedby="password_confirm-error">
+
+                    <?php if (isset($errors['password_confirm'])): ?>
+                        <span class="error" id="password_confirm-error" role="alert">
+                            <?= $errors['password_confirm']; ?>
+                        </span>
+                    <?php endif; ?>
+                </div>
+
+                <button type="submit">Register</button>
             </form>
         </div>
     </div>
