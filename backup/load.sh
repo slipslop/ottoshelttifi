3170f8e4dfc30502c076a25516aabbb5ac0582f6
slipslop <vili.takaanttila@gmail.com>
2025-08-30 10:53:49 +0000

    Update load script

--- backup/load.sh
+++ backup/load.sh
@@ -1,2 +1,6 @@
-docker exec mariadb \
-    mariadb-import -u root -p  ottoshelttifi < ./backup.sql
+#!/bin/bash
+
+DB_PASS=$(< ../docker/secrets/db_password.txt)
+
+docker exec -e MYSQL_PWD="$DB_PASS" -i  mariadb \
+  mariadb -u root ottoshelttifi < ./backup.sql
